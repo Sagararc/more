0c7b6ca60f6373ab50b90faa42e593fa35734b24
gd388 <105911071+Sagararc@users.noreply.github.com>
2023-09-03 23:05:54 +0530

    sacasd

--- core/settings.py
+++ core/settings.py
@@ -122,8 +122,10 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 
