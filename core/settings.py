f26adbe0dac8b76f1ee7d08e713c66e2eb29c7aa
gd388 <105911071+Sagararc@users.noreply.github.com>
2023-09-11 14:35:26 +0530

    final

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-7z&^x!veou(8=$_=1viia)_bnm_^%h0f@29-kkh)awo$hwh!d2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.105.56.121']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'more',
-    'adminArea',
+    'corsheaders',
+    
   
    
 ]
@@ -40,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -122,5 +124,8 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+  'http://172.105.56.121:8001/',
+)
 
